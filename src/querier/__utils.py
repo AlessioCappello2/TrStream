2070ee837bc7c77b554279f878a6e976f5b89fd8
Alessio Cappello s309450 <alessio.cappello@studenti.polito.it>
2025-12-02 23:47:34 +0100

    first sql editor prototype

--- src/querier/__utils.py
+++ src/querier/__utils.py
@@ -1,3 +1,4 @@
+import re
 
 from pydantic import BaseModel
 
@@ -21,3 +22,25 @@ def is_safe_query(sql: str) -> bool:
             normalized.startswith("with")) 
         and not any(f in normalized for f in forbidden)
     )
+
+def clean_error_message(e: Exception) -> str:
+    msg = str(e)
+
+    lines = [line.strip() for line in msg.split("\n") if line.strip()]
+    msg = msg if not lines else lines[0]
+
+    prefixes = {
+        r"^DuckDB Error:\s*",
+        r"^HTTP Error:\s*",
+        r"^Catalog Error:\s*",
+        r"^IO Error:\s*",
+        r"^Binder Error:\s*",
+        r"^Parser Error:\s*",
+        r"^\[\d+\]:\s*",
+        r"^\d{3}\s*[:\-]\s*",
+    }
+
+    for p in prefixes:
+        msg = re.sub(p, '', msg)
+
+    return msg
--- src/streamlit/app.py
+++ src/streamlit/app.py
@@ -1,30 +1,86 @@
 import streamlit as st
 import requests
 import pandas as pd
+import threading
+import time
+
+API_BASE = "http://querier:8000"
 
 st.set_page_config(page_title="DuckDB SQL Editor", layout="wide")
 st.title("DuckDB SQL Editor")
 
-# User input SQL
-sql = st.text_area("Enter SQL query:", height=150)
+tab_query, tab_alias = st.tabs(["Run Query", "Manage Aliases"])
 
-if st.button("Run Query"):
-    if not sql.strip():
-        st.warning("Please enter a SQL query")
-    else:
-        try:
-            # Send query to FastAPI backend
+# Tab query
+with tab_query:
+    st.subheader("Run SQL query")
+    sql = st.text_area("Enter SQL query:", height=150)
+
+    if st.button("Run Query"):
+        if not sql.strip():
+            st.warning("Please enter a SQL query")
+        else:
+            try:
+                placeholder = st.empty()
+                start = time.perf_counter()
+                result_container = {"response": None}
+
+                def run_query():
+                    try: 
+                        result_container["response"] = requests.post(
+                                                        f"{API_BASE}/query",
+                                                        json={"sql": sql},
+                                                    )
+                    except Exception:
+                        result_container["response"] = e
+                        raise Exception
+                    
+                t = threading.Thread(target=run_query)
+                t.start()
+
+                while t.is_alive():
+                    elapsed = time.perf_counter() - start 
+                    placeholder.info(f"Running... {elapsed:.2f} sec")
+                    time.sleep(0.1)
+
+                t.join()
+                placeholder.empty()
+
+                response = result_container["response"]
+                result = response.json()
+                response.raise_for_status()
+
+                placeholder.success(f"Completed in {elapsed:.2f} sec - {result['rows']} rows returned")
+                df = pd.DataFrame(result['data'])
+                st.dataframe(df)
+            except Exception as e:
+                try:
+                    detail = result_container["response"].json()["detail"]
+                except Exception:
+                    detail = "Unknown internal error"
+                st.error(detail)
+
+# Tab alias
+with tab_alias:
+    st.subheader("Create or Update Alias")
+
+    alias_name = st.text_input("Alias name (e.g. tx)", key="alias_name")
+    alias_path = st.text_input("Parquet path (e.g. s3://bucket/path/*.parquet)", key="alias_path")
+
+    if st.button("Create Alias"):
+        if not alias_name or not alias_path:
+            st.error("Both alias name and path are required.")
+        else:
             response = requests.post(
-                "http://querier:8000/query",
-                json={"sql": sql},
+                f"{API_BASE}/alias",
+                json={"name": alias_name, "path": alias_path}
             )
-            response.raise_for_status()
-            result = response.json()
 
-            st.success(f"{result['rows']} rows returned")
-            df = pd.DataFrame(result['data'])
-            st.dataframe(df)
-        except requests.exceptions.RequestException as e:
-            st.error(f"Failed to run query: {e}")
-        except Exception as e:
-            st.error(f"Error: {e}")
+            if response.status_code == 200:
+                st.success(response.json()["detail"])
+            else:
+                try:
+                    detail = response.json().get("detail", "Unknown internal error")
+                except Exception:
+                    detail = response.text
+                st.error(detail)
